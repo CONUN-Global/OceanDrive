74d7fc2a43f0318d240d3d52f99ed188fd01ca73
otabek <splabdev@rfalab.com>
2022-05-12 11:08:19 +0500

    react query setup

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { QueryClientProvider } from 'react-query';
+import { Route, Routes } from 'react-router-dom';
 import Layout from './components/Layout';
+import { queryClient } from './config/queryClient';
 import Main from './pages/Main';
 import './styles/globals.scss';
 
@@ -8,7 +10,9 @@ function App() {
   return (
     <Layout>
       <Routes>
-        <Route path="/" element={<Main />} />
+        <QueryClientProvider client={queryClient}>
+          <Route path="/" element={<Main />} />
+        </QueryClientProvider>
       </Routes>
     </Layout>
   );
