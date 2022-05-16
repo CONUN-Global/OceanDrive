767cc61b0c489d305bd74bbeda52207776191a3b
IslomJonMirzakbarov <islomjonmirzakbarov@gmail.com>
2022-05-16 09:49:16 +0900

    resolved rebase confilcts

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { ReactQueryDevtools } from 'react-query/devtools';
 import { QueryClientProvider } from 'react-query';
-import { useRoutes } from 'react-router-dom';
+import { Route, Routes, useRoutes } from 'react-router-dom';
 import Layout from './components/Layout';
 import { queryClient } from './config/queryClient';
 import { onboardingRoutes } from './routes/wallet';
 import './styles/globals.scss';
+import Storage from './pages/Storage/Storage';
+import Main from './pages/Main';
 
 function App() {
   // it will be divided into authorized (user with wallet)
@@ -15,9 +17,15 @@ function App() {
   const routes = useRoutes(filteredRoutes);
 
   return (
-    <Layout>
-      <QueryClientProvider client={queryClient}>{routes}</QueryClientProvider>
-    </Layout>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/storage" element={<Storage />} />
+        </Routes>
+      </Layout>
+    </QueryClientProvider>
   );
 }
 
