7d52684e54a082b4aeae5d3641b4d39ae3e5bd94
IslomJonMirzakbarov <islomjonmirzakbarov@gmail.com>
2022-05-17 15:58:37 +0900

    initialized demo filled items storage page

---
+++ apps/web/src/pages/StorageFilledDemo/StorageFilledDemo.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import styles from './StorageFilledDemo.module.scss';
+
+const StorageFilledDemo = () => {
+  return (
+    <div>StorageFilledDemo</div>
+  )
+}
+
+export default StorageFilledDemo
