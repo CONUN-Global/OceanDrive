a4457330b94c6628c5273452d8cee8d4a94b284f
otabek <splabdev@rfalab.com>
2022-05-18 09:21:59 +0500

    tested ipfs

--- apps/desktop/ipfs/index.js
+++ apps/desktop/ipfs/index.js
@@ -1,12 +1,20 @@
 const IPFS = require('ipfs-core');
-const { executeWeb3Storage } = require('@ocean-drive/core');
-
+const { libp2pBundle, Web3Storage } = require('@conun-global/web3.storage');
+const { executeWeb3Storage } = require('../../core/lib/esm');
+// import { libp2pBundle, Web3Storage } from "@conun-global/web3.storage";
 let node;
 
 async function createIpfs() {
-  executeWeb3Storage().then((res) => {
-    console.log('res ===> ', res);
-  });
+  try {
+    // const storage = new Web3Storage(await libp2pBundle());
+    // await storage.startUp();
+    await executeWeb3Storage().then((res) => {
+      console.log('res ====> ', res);
+    });
+  } catch (error) {
+    console.log('error: ', error);
+  }
+
   // try {
   //   node = await IPFS.create();
   //   const id = await node.id();
