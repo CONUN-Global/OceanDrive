e7d6befae61b1867bf8ac1f57aae8b2eee365e2a
IslomJonMirzakbarov <islomjonmirzakbarov@gmail.com>
2022-05-23 16:32:19 +0900

    added scroll function to marketplace single item page

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -21,7 +21,7 @@ function App() {
   const persistor = persistStore(store);
 
   // in order to see storage and marketplace pages conditionally rendering routes, later we may remove test after implementing full onboarding pages
-  const test = false;
+  const test = true;
 
   // it will be divided into authorized (user with wallet)
   // and non authorized that will pass through onboarding process
