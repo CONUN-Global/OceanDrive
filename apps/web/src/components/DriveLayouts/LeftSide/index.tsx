20808dda9ba60c1876b3c77c4d5ba795f6b38cec
Daniel <dbjowett@gmail.com>
2022-05-23 18:08:13 +0900

    Complete Single Item page, fix settings and logout icons

--- apps/web/src/components/DriveLayouts/LeftSide/index.tsx
+++ apps/web/src/components/DriveLayouts/LeftSide/index.tsx
@@ -1,5 +1,8 @@
+import classnames from 'classnames';
 import React, { ReactNode } from 'react';
-import { ReactComponent as UnionVectorIcon } from '../../../assets/icons/union-vector-icon.svg';
+import { ReactComponent as UnionVectorIcon } from 'src/assets/icons/union-vector-icon.svg';
+import { ReactComponent as SettingsIcon } from 'src/assets/icons/settings-icon.svg';
+
 import placeholderImg from '../../../assets/images/Profile.png';
 import Button from '../../Button';
 import styles from './LeftSidebar.module.scss';
@@ -13,11 +16,13 @@ const LeftSidebar = ({ children }: { children?: ReactNode }) => {
         <div>
           <Button className={styles.UploadButton}>Upload a File</Button>
           <div className={styles.UtilityButtons}>
-            <div className={styles.LockAccount}>
-              Lock <UnionVectorIcon />
+            <div className={classnames(styles.Logout, styles.UtilItem)}>
+              <UnionVectorIcon />
+              Logout
             </div>
-            <div className={styles.LockAccount}>
-              Settings <UnionVectorIcon />
+            <div className={classnames(styles.Settings, styles.UtilItem)}>
+              <SettingsIcon />
+              Settings
             </div>
           </div>
         </div>
