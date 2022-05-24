c9e1b4582aa97b543d331d6052718903cc7ad6b4
Daniel <dbjowett@gmail.com>
2022-05-24 11:16:12 +0900

    Remove SplashScreen Page

--- apps/web/src/pages/SplashScreen/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import SplashScreen from 'src/components/SplashScreen';
-
-function SplashScreenPage() {
-  return <SplashScreen />;
-}
-
-export default SplashScreenPage;
