2c76277ecdd03753b88af83c3126e83cda0108c4
IslomJonMirzakbarov <islomjonmirzakbarov@gmail.com>
2022-05-25 18:27:15 +0900

    added some features of wallet transaction page

--- apps/web/src/pages/Wallet/TransComponents/Transactions/index.tsx
+++ apps/web/src/pages/Wallet/TransComponents/Transactions/index.tsx
@@ -1,7 +1,24 @@
 import React from 'react';
 
+import styles from './Transactions.module.scss';
+
+const data = [3];
+
 function Transactions() {
-  return <div>Transactions</div>;
+  return (
+    <div className={styles.Container}>
+      <table className={styles.Table}>
+        <tr className={styles.TableRow}>
+          <th>Transaction ID</th>
+          <th>Status</th>
+          <th>Date</th>
+          <th>Amount</th>
+        </tr>
+        {data.length > 0 && <p className={styles.P}>data</p>}
+      </table>
+      {data.length === 0 && <div className={styles.NoData}>NO DATA</div>}
+    </div>
+  );
 }
 
 export default Transactions;
