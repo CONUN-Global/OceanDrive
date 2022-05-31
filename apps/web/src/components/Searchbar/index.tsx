57f740026dc3af3237036933488b2030c7327879
Daniel <dbjowett@gmail.com>
2022-05-31 16:39:50 +0900

    Fix bug which changed colour of search icon

--- apps/web/src/components/Searchbar/index.tsx
+++ apps/web/src/components/Searchbar/index.tsx
@@ -1,11 +1,8 @@
 import React, { useState } from 'react';
 import styles from './Searchbar.module.scss';
 import { ReactComponent as SearchIcon } from 'src/assets/icons/search_icon.svg';
-import { useLocation } from 'react-router-dom';
 
 function Searchbar() {
-  const { pathname } = useLocation();
-
   const [searchTerm, setSearchTerm] = useState<string>('');
 
   function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
