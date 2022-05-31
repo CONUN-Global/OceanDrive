1a60f1eecc311686f325b04ca2207b90245bf58e
Daniel <dbjowett@gmail.com>
2022-05-31 18:03:18 +0900

    Merged with Single-page-responsiveness

---
+++ apps/packages/index.tsx
@@ -0,0 +1,48 @@
+import classNames from 'classnames';
+import React, { ReactNode } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { ReactComponent as OceanStorageIcon } from '../../../assets/icons/OceanStorage-icon.svg';
+
+import { ReactComponent as MarketIcon } from '../../../assets/icons/Sidebar/Market.svg';
+import { ReactComponent as WalletIcon } from '../../../assets/icons/Sidebar/Wallet.svg';
+import { ReactComponent as PublishIcon } from '../../../assets/icons/Sidebar/Publish.svg';
+import { ReactComponent as HostIcon } from '../../../assets/icons/Sidebar/Host.svg';
+
+import styles from './RightSideLayer.module.scss';
+
+interface IRightSide {
+  children: ReactNode;
+}
+
+const IconArray = [
+  { title: 'marketplace', icon: <MarketIcon /> },
+  { title: 'wallet', icon: <WalletIcon /> },
+  { title: 'publish', icon: <PublishIcon /> },
+  { title: 'host', icon: <HostIcon /> },
+];
+
+function RightSideLayer({ children }: IRightSide) {
+  const { pathname } = useLocation();
+  const navigate = useNavigate();
+
+  return (
+    <div className={styles.OuterBg}>
+      <div className={styles.InnerBg}>
+        <div className={styles.TopBar}>
+          <div className={styles.IconContainer}>
+            {IconArray.map((item, index) => {
+              return (
+                <div key={index} onClick={() => navigate(`/${item.title}`)} className={classNames(styles.BtnContainer, { [styles.active]: pathname.includes(`${item.title}`) })}>
+                  {item.icon}
+                </div>
+              );
+            })}
+          </div>
+        </div>
+        {children}
+      </div>
+    </div>
+  );
+}
+
+export default RightSideLayer;
