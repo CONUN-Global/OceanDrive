eb91ca29ae58ac96711a8e9c17077cc3518eb3f0
Daniel <dbjowett@gmail.com>
2022-06-07 14:21:24 +0900

    Finalize Onboarding CSS

--- apps/web/src/components/Backdrop/index.tsx
+++ apps/web/src/components/Backdrop/index.tsx
@@ -1,11 +1,13 @@
+import classNames from 'classnames';
 import React from 'react';
 import styles from './Backdrop.module.scss';
 interface Props {
   isModalOpen: boolean;
   setIsModalOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  isClear?: boolean;
 }
-function Backdrop({ setIsModalOpen, isModalOpen }: Props) {
-  return <div onClick={() => setIsModalOpen(false)} className={isModalOpen ? styles.Open : styles.Closed}></div>;
+function Backdrop({ setIsModalOpen, isModalOpen, isClear }: Props) {
+  return <div onClick={() => setIsModalOpen(false)} className={classNames(isModalOpen ? styles.Open : styles.Closed, { [styles.Clear]: isClear })}></div>;
 }
 
 export default Backdrop;
