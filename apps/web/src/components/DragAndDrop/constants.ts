a4d21b686da34c323fd2fb715fd1374cb0472dbf
dbjowett <dbjowett@gmail.com>
2022-06-20 09:59:46 +0900

    Modify constants

--- apps/web/src/components/DragAndDrop/constants.ts
+++ apps/web/src/components/DragAndDrop/constants.ts
@@ -1,19 +1,17 @@
-export const ERRORS = {
-  SIZE_ERR: {
-    CODE: 'file-too-large',
-    MSG: 'File too large. Please select a smaller file.',
-  },
-  COUNT_ERR: {
-    CODE: 'too-many-files',
-    MSG: 'You have selected too many files. Please use only a single photo for the thumbnail.',
-  },
-  INVALID_ERR: {
-    CODE: 'file-invalid-type',
-    MSG: 'This is an invalid file type. Please select a JPEG or PNG.',
-  },
-  DEFAULT_ERR: {
-    MSG: 'Something went wrong, please try again.',
-  },
+export const SIZE_ERR = {
+  CODE: 'file-too-large',
+  MSG: 'File too large. Please select a smaller file.',
+};
+export const COUNT_ERR = {
+  CODE: 'too-many-files',
+  MSG: 'You have selected too many files. Please use only a single photo for the thumbnail.',
+};
+export const INVALID_ERR = {
+  CODE: 'file-invalid-type',
+  MSG: 'This is an invalid file type. Please select a JPEG or PNG.',
+};
+export const DEFAULT_ERR = {
+  MSG: 'Something went wrong, please try again.',
 };
 
 // MIME Type + [file extensions to accept]
