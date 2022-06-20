04aaa6d375f0363ac9eae14ecc0976e8848cbdb1
dbjowett <dbjowett@gmail.com>
2022-06-20 18:05:13 +0900

    Begin pw reset

--- apps/web/src/pages/Settings/ChangePass/index.tsx
+++ apps/web/src/pages/Settings/ChangePass/index.tsx
@@ -6,10 +6,21 @@ import styles from './ChangePass.module.scss';
 function ChangePass() {
   return (
     <SettingsCard title="Change Password">
-      <div className={styles.BtnContainer}>
-        <Button variant="secondary" className={styles.Button}>
-          Continue
-        </Button>
+      <div className={styles.Container}>
+        <div className={styles.InputsContainer}>
+          <input placeholder="Enter your current password" type="password" name="" id="" />
+          <input placeholder="Enter a new password" type="password" name="" id="" />
+          <input placeholder="Confirm your new password" type="password" name="" id="" />
+        </div>
+
+        <div className={styles.BtnContainer}>
+          <Button variant="secondary" className={styles.CancelButton}>
+            Cancel
+          </Button>
+          <Button variant="secondary" className={styles.Button}>
+            Change
+          </Button>
+        </div>
       </div>
     </SettingsCard>
   );
