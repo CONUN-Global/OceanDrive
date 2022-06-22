8d28c90365f34662c3d2fbede680b632dd0aa384
dbjowett <dbjowett@gmail.com>
2022-06-22 10:54:04 +0900

    Error password length error, fix use of global variables around application

--- apps/web/src/hooks/useValidateNewPassword.tsx
+++ apps/web/src/hooks/useValidateNewPassword.tsx
@@ -4,6 +4,8 @@ interface IErrorMessages {
   confirmError: string;
 }
 
+// Takes an Error Messages object (above), takes in a users password we have stored, the users current password as typed by the user,
+// a new password, and a match copy of the new password
 function useValidate(errs: IErrorMessages, userPassword: string, currentPass: string, newPass: string, confirmPass: string) {
   if (currentPass !== userPassword) {
     errs = { ...errs, current: 'Provided password is incorrect.' };
@@ -17,10 +19,17 @@ function useValidate(errs: IErrorMessages, userPassword: string, currentPass: st
     errs = { ...errs, newError: '' };
   }
 
+  // New passwords must not be over 50
+  if (newPass.length > 50 || confirmPass.length > 50) {
+    errs = { ...errs, confirmError: 'Enter a password that is maximum 50 characters.', newError: 'Enter a password that is maximum 50 characters.' };
+  } else {
+    errs = { ...errs, newError: '', confirmError: '' };
+  }
+
   // New password and Confirm password don't match
   if (newPass !== confirmPass) {
-    errs = { ...errs, confirmError: "Provided password doesn't match" };
-  } else {
+    errs = { ...errs, confirmError: "Provided passwords don't match.", newError: "Provided passwords don't match." };
+  } else if (confirmPass.length < 50 || newPass.length < 50) {
     errs = { ...errs, confirmError: '' };
   }
 
