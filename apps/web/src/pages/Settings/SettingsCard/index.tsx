a802049d5340a4b2d892fbe7ebd28d4a200060c3
dbjowett <dbjowett@gmail.com>
2022-06-22 11:50:33 +0900

    Fix bug where all svgs were appearing blue >:(

--- apps/web/src/pages/Settings/SettingsCard/index.tsx
+++ apps/web/src/pages/Settings/SettingsCard/index.tsx
@@ -8,8 +8,8 @@ function SettingsCard({ children, title, isLarge = false }: { children: React.Re
   const navigate = useNavigate();
   return (
     <div className={classNames(styles.Container, { [styles.LgContainer]: isLarge })}>
-      <button className={styles.XBtn} onClick={() => navigate(-1)}>
-        <Close />
+      <button className={styles.XBtnContainer} onClick={() => navigate(-1)}>
+        <Close className={styles.XBtn} />
       </button>
       <div className={styles.ContentContainer}>
         <h1 className={styles.Title}>{title}</h1>
