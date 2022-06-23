825134e22bff573dee4012f3bb1e69eac6824d00
dbjowett <dbjowett@gmail.com>
2022-06-23 15:33:49 +0900

    Changes to Validate Password

--- apps/web/src/hooks/useValidateNewPassword.tsx
+++ apps/web/src/hooks/useValidateNewPassword.tsx
@@ -1,34 +1,27 @@
-interface IErrorMessages {
-  current: string;
-  newError: string;
-  confirmError: string;
-}
+const ErrMsg = {
+  incorrect: 'Provided password is incorrect.',
+  new: 'Enter a password that you have not used before.',
+  length: 'Enter a password that is maximum 50 characters.',
+  match: "Provided passwords don't match.",
+};
 
-// Takes an Error Messages object (above), takes in a users password we have stored, the users current password as typed by the user,
-// a new password, and a match copy of the new password
+let errs = { current: '', newError: '', confirmError: '' };
+
 function useValidate(userPassword: string, currentPass: string, newPass: string, confirmPass: string) {
-  let errs = { current: '', newError: '', confirmError: '' };
   if (currentPass !== userPassword) {
-    errs = { ...errs, current: 'Provided password is incorrect.' };
-  } else {
-    errs = { ...errs, current: '' };
+    errs = { ...errs, current: ErrMsg.incorrect };
   }
 
   if (newPass === userPassword) {
-    // REUSED PASS
-    errs = { ...errs, newError: 'Enter a password that you have not used before.' };
-  } else if (newPass.length > 50 || confirmPass.length > 50) {
-    // OVER 50 CHARS
-    errs = { ...errs, confirmError: 'Enter a password that is maximum 50 characters.', newError: 'Enter a password that is maximum 50 characters.' };
-  } else {
-    errs = { ...errs, newError: '' };
+    errs = { ...errs, newError: ErrMsg.new };
   }
 
-  // New password and Confirm password don't match
+  if (newPass.length > 50 || confirmPass.length > 50) {
+    errs = { ...errs, confirmError: ErrMsg.length, newError: ErrMsg.length };
+  }
+
   if (newPass !== confirmPass) {
-    errs = { ...errs, confirmError: "Provided passwords don't match.", newError: "Provided passwords don't match." };
-  } else if (confirmPass.length < 50 || newPass.length < 50) {
-    errs = { ...errs, confirmError: '' };
+    errs = { ...errs, confirmError: ErrMsg.match, newError: ErrMsg.match };
   }
 
   return errs;
