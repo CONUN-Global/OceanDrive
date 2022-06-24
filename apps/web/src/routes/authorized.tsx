a28f8b16dfcd24f8eb5eafb7a4aa4a45869f593b
otabek <splabdev@rfalab.com>
2022-06-24 10:58:30 +0500

    build file configured

--- apps/web/src/routes/authorized.tsx
+++ apps/web/src/routes/authorized.tsx
@@ -4,9 +4,9 @@ import MarketPlace from '../pages/Marketplace';
 import SingleItem from '../pages/SingleItem';
 import SplashScreen from '../pages/SplashScreen';
 import Storage from '../pages/Publish/Publish';
-import Profile from 'src/pages/Profile';
-import Host from 'src/pages/Host';
-import DriveLayout from 'src/components/DriveLayouts';
+import Profile from '../pages/Profile';
+import Host from '../pages/Host';
+import DriveLayout from '../components/DriveLayouts';
 
 export const authorizedRoutes = [
   {
