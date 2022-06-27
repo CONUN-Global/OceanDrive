299ff2ac5fcc64cf00590c6fa985756650b63fb8
otabek <splabdev@rfalab.com>
2022-06-27 07:53:51 +0500

    minimal height and width given , login logout added

--- apps/web/src/components/Searchbar/index.tsx
+++ apps/web/src/components/Searchbar/index.tsx
@@ -1,13 +1,9 @@
 import React, { useState } from 'react';
 import styles from './Searchbar.module.scss';
-import { ReactComponent as SearchIcon } from 'src/assets/icons/search_icon.svg';
-import useGetImage from 'src/hooks/useGetImage';
+import { ReactComponent as SearchIcon } from '../../assets/icons/search_icon.svg';
 
 function Searchbar() {
   const [searchTerm, setSearchTerm] = useState<string>('');
-  const { data } = useGetImage(searchTerm);
-
-  console.log(data);
 
   function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
     setSearchTerm(e.target.value);
@@ -21,7 +17,6 @@ function Searchbar() {
         </div>
         <input value={searchTerm} className={styles.Input} type="text" placeholder="Search" onChange={handleChange} />
       </div>
-      {data && <img src={data} width={50} height={50} />}
     </>
   );
 }
